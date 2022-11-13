77c8f85565510a9723a0c9701355573925a63c71
jande <jande@DESKTOP-NQL6GK8>
2022-11-13 18:03:48 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela.API/Program.cs
@@ -0,0 +1,41 @@
+using DevFreela.API.Models;
+using DevFreela.Application.Services.Implementations;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<OpeningTimeOption>(builder.Configuration.GetSection("OpeningTime"));
+
+var connectionString = builder.Configuration.GetConnectionString("DevFreelaCs");
+builder.Services.AddDbContext<DevFreelaDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IProjectService, ProjectService>();
+
+builder.Services.AddScoped<ExampleClass>(e => new ExampleClass { Name = "Initial Stage" });
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ DevFreela.Core/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace DevFreela.Core.Entities
+{
+    // Esta classe não é instanciada.
+    public abstract class BaseEntity
+    {
+        public BaseEntity()
+        {
+
+        }
+        public int Id { get; private set; }
+    }
+}
